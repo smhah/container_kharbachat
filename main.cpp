d698d28478c14dea3878aa93530558ddc538914d
Salah Eddine Mhah <smhah@e1r2p6.1337.ma>
2022-03-25 19:02:27 +0100

    s

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: smhah <smhah@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/13 16:37:43 by smhah             #+#    #+#             */
-/*   Updated: 2022/03/24 20:11:35 by smhah            ###   ########.fr       */
+/*   Updated: 2022/03/25 19:01:18 by smhah            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,38 @@
 
 int main(int ac, char **av)
 {
-    ft::vector<char> test;
-    std::vector<char> testo;
-    std::vector<int> foo (3,100);   // three ints with a value of 100
-    std::vector<int> bar (5,200);
-    std::vector<int>::iterator  ito; 
-    ft::vector<char>::iterator  it;
+    ft::vector<int> foo;   // three ints with a value of 100
+    std::vector<int> bar;  // five ints with a value of 200
 
-    std::cout << "HELLO SALAH" << std::endl;
-    ito = foo.begin();
-    foo.insert(it, 2, 150);
-    for(int i = 0;i < foo.size();i++)
-        std::cout << foo[i] << std::endl;
     
+    // foo.swap(bar);
+    foo.push_back(1);
+    foo.push_back(2);
+    foo.push_back(3);
+    ft::vector<int>::iterator itb;
+    ft::vector<int>::iterator ite;
+    itb = foo.begin();
+    ite = foo.begin() + 1;
+    ite = foo.erase(itb, ite);
+    //itb = foo.erase(itb);
+
+    bar.push_back(1);
+    bar.push_back(2);
+    bar.push_back(3);
+    std::vector<int>::iterator itob;
+    std::vector<int>::iterator itoe;
+    itob = bar.begin() + 1;
+    itoe = bar.begin() + 2;
+    itob = bar.erase(itob, itoe);
+    std::cout << "foo contains:";
+    for (unsigned i=0; i<foo.size(); i++)
+    std::cout << ' ' << foo[i];
+    std::cout << '\n';
+    
+    std::cout << "bar contains:";
+    for (unsigned i=0; i<bar.size(); i++)
+    std::cout << ' ' << bar[i];
+    std::cout << '\n';
+
+    return 0;
 }
--- vector.hpp
+++ vector.hpp
@@ -6,15 +6,19 @@
 /*   By: smhah <smhah@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/13 16:33:18 by smhah             #+#    #+#             */
-/*   Updated: 2022/03/24 20:06:49 by smhah            ###   ########.fr       */
+/*   Updated: 2022/03/25 19:01:58 by smhah            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef VECTOR_HPP
+# define VECTOR_HPP
+
 #include <iostream>
 #include "iterator_traits.hpp"
-//#include <iterator>
-
-
+#include <stdio.h>
+#include <iterator>
+#include <algorithm>
+#pragma once
 namespace ft
 {
 	template < class T, class Alloc = std::allocator<T> > 
@@ -24,12 +28,12 @@ namespace ft
 			typedef	T	value_type;
 			typedef Alloc allocator_type;
 			typedef size_t	size_type;
-			typedef ft::iterator_op<T> iterator;
-			typedef typename allocator_type::pointer	pointer;
-			typedef typename allocator_type::const_pointer	const_pointer;
-			typedef typename allocator_type::reference	reference;
-			typedef typename allocator_type::const_reference const_reference;
-			
+			typedef	ft::iterator_op<T> iterator;
+			typedef typename	allocator_type::pointer	pointer;
+			typedef typename	allocator_type::const_pointer	const_pointer;
+			typedef typename	allocator_type::reference	reference;
+			typedef typename	allocator_type::const_reference const_reference;
+			typedef typename	std::ptrdiff_t							difference_type;
 			//_________exceptions__________________
 			class outOfBound : public std::exception
 			{
@@ -47,25 +51,24 @@ namespace ft
 			vector(const allocator_type & alloc= allocator_type())
 			{
 				std::cout << "default constructor called" << std::endl;
-				//std::cout << "alloc is :" << alloc << std::endl;
-				//this->_content = this->allocator.allocate(100);
-				//this->_content = NULL;
-				this->_capacity = 2;
-				this->_content = this->_allocator.allocate(sizeof(value_type) * 2);
-				//this->_allocator = nullptr;
+				_capacity = 2;
+				this->_content = this->_allocator.allocate(_capacity);
 				this->_size = 0;
-				//this->_content = new value_type[100];
-				//this->_size = 100;
 			};
 			explicit vector (size_type n, const value_type& val = value_type(),
 					const allocator_type& alloc = allocator_type())
 			{
-				std::cout << "second default constructor called" << std::endl;
-				this->_size = n;
-				this->_content = this->_allocator.allocate(sizeof(value_type) * (n + 1));
-				for (int i = 0; i < n;i++)
+				_capacity = 2;
+				_content = _allocator.allocate(_capacity);
+				_size = 0;
+				while(n > _capacity)
+					realloc_capacity();
+				int i = 0;
+				while(i < n)
 				{
-					this->_content[i] = val;
+					_allocator.construct(_content + i, val);
+					i++;
+					_size++;
 				}
 			}
 			~vector()
@@ -94,6 +97,44 @@ namespace ft
 			}
 
 			//_________member functions________________
+
+			iterator erase (iterator position)
+			{
+				iterator save_position = position;
+				iterator next;
+				for(;position < end() - 1;position++)
+				{
+					next = position + 1;
+					std::swap(*position.get_pointer(), *next.get_pointer());
+				}
+				pop_back();
+				return (save_position);
+			}
+			
+			iterator erase (iterator first, iterator last)
+			{
+				iterator save_first = first;
+				size_type distance = std::distance(begin(), end());
+				for(;first < end() - 1;first++)
+				{
+					_allocator.destroy(_content);
+				}
+			}
+
+			void swap(vector & x)
+			{
+				pointer content = _content;
+				size_type size = _size;
+				size_type capacity = _capacity;
+
+				_content = x._content;
+				_size = x._size;
+				_capacity = x._capacity;
+
+				x._content = content;
+				x._size = size;
+				x._capacity = capacity;
+			}
 
 			void resize (size_type n, value_type val = value_type())
 			{
@@ -142,7 +183,6 @@ namespace ft
 			void	realloc_capacity(void)
 			{
 				value_type *tmp;
-
 				size_type old_capacity = _capacity;
 
 				_capacity *= 2;
@@ -232,6 +272,9 @@ namespace ft
 			{
 				pointer tmp;
 
+				// size_type distance = std::distance(begin(), position);
+				size_type distance = std::distance(begin(), position);
+				std::cout << "distance : " << distance << std::endl;
 				if(_size + 1 >= _capacity)
 					realloc_capacity();
 				tmp = _allocator.allocate(_capacity);
@@ -249,7 +292,7 @@ namespace ft
 						}								
 						_allocator.construct(tmp + j, val);
 						j++;
-						while(i < size())
+						while(i < size() - 1)
 						{
 							_allocator.construct(tmp + j, _content[i]);
 							i++;
@@ -271,6 +314,59 @@ namespace ft
 				_content = tmp;
 				_allocator.deallocate(tmp, _capacity);
 				return (position);
+			}
+
+			void	insert (iterator position, size_type n , const value_type& val)
+			{
+				pointer tmp;
+
+				if(_size + 1 >= _capacity)
+					realloc_capacity();
+				tmp = _allocator.allocate(_capacity);
+				int i = -1;
+				while(++i < size())
+				{
+					if(compare_pointers(position.get_pointer(), _content + i))
+					{
+						std::cout << "ENTERED" << std::endl;
+						_size += n;
+						int j = 0;
+						while(j < i)
+						{
+							_allocator.construct(tmp + j, _content[j]);
+							j++;
+						}
+						int c = n;
+						while(c--)
+						{
+							_allocator.construct(tmp + j, val);
+							j++;
+						}
+						while(i < size() - n)
+						{
+							_allocator.construct(tmp + j, _content[i]);
+							std::cout <<"content[" << i << "] = " <<_content[i] << std::endl;
+							std::cout <<"tmp[" << j << "] = " <<tmp[j] << std::endl;
+							i++;
+							j++;
+						}
+						break;
+					} 
+					else if(i + 1 == size())
+					{
+						if(compare_pointers(position.get_pointer() - 1, _content + i))
+						{
+							while(n--)
+								this->push_back(val);
+							_allocator.deallocate(tmp, _capacity);
+							return ;
+						}
+					}
+				}
+				_allocator.deallocate(_content, _capacity);
+				_content = tmp;
+				_allocator.deallocate(tmp, _capacity);
+				return ;
 			}
 			//_________getters________________
 			
@@ -306,3 +402,4 @@ namespace ft
 	// 	}
 	// }
 }; // end of namespace
+#endif
